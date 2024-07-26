ef7c3165d133822cadb188e6655b4ae4a818ef92
enoumy <enoumy@gmail.com>
2024-07-26 00:13:34 -0400

    Finished clock example

--- lib/capytui/examples/clock_example/main.ml
+++ lib/capytui/examples/clock_example/main.ml
@@ -3,6 +3,26 @@ open! Bonsai
 open Capytui
 open Bonsai.Let_syntax
 
+let ( >> ) a b = Fn.compose a b
+
+let render_date (date : Date.t) =
+  let t f =
+    Node.text
+      ~attrs:[ Attr.foreground_color Attr.Color.blue ]
+      (Int.to_string (f date))
+  in
+  let year = t Date.year in
+  let month = t (Month.to_int >> Date.month) in
+  let day = t Date.day in
+  Node.hcat [ year; Node.text "-"; month; Node.text "-"; day ]
+;;
+
+let render_time_of_day (time_of_day : Time_ns.Ofday.t) =
+  Node.text
+    ~attrs:[ Attr.bold; Attr.foreground_color Attr.Color.green ]
+    (Time_ns.Ofday.to_sec_string time_of_day)
+;;
+
 let clock_app =
   let%sub now =
     Bonsai.Clock.approx_now ~tick_every:(Time_ns.Span.of_sec 1.0)
@@ -10,9 +30,13 @@ let clock_app =
   let%sub dimensions = Capy.terminal_dimensions in
   let%arr { width; height } = dimensions
   and now = now in
+  let date = Time_ns.to_date ~zone:(force Timezone.local) now in
+  let date = render_date date in
+  let time_of_day = Time_ns.to_ofday ~zone:(force Timezone.local) now in
+  let time_of_day = render_time_of_day time_of_day in
   Node.center
-    (Node.text (Time_ns.to_string_utc now))
     ~within:{ width; height }
+    (Node.hcat [ date; Node.text " "; time_of_day ])
 ;;
 
 let command =
