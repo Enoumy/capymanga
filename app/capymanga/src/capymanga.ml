488d032d6dd8763f70f4c348b4387e03f82b5b72
enoumy <enoumy@gmail.com>
2024-07-31 21:05:26 -0400

    z

--- app/capymanga/src/capymanga.ml
+++ app/capymanga/src/capymanga.ml
@@ -2,6 +2,7 @@ open! Core
 open Bonsai
 open Bonsai.Let_syntax
 open Capytui
+module Catpuccin = Capytui_catpuccin
 
 let backdrop =
   let%sub dimensions = Capytui.terminal_dimensions in
@@ -15,7 +16,19 @@ let backdrop =
          (String.make width ' ')))
 ;;
 
-let image =
+let sexp_for_debugging =
+  let%sub text = Catpuccin.color Text in
+  let%sub crust = Catpuccin.color Crust in
+  let%arr text = text
+  and crust = crust in
+  fun ?(attrs = []) sexp ->
+    Node.sexp_for_debugging
+      ~attrs:
+        ([ Attr.background_color crust; Attr.foreground_color text ] @ attrs)
+      sexp
+;;
+
+let _image =
   let%sub dimensions = Capytui.terminal_dimensions in
   let%sub () =
     Bonsai.Edge.on_change
@@ -35,14 +48,70 @@ let image =
   Bonsai.const ()
 ;;
 
+let num_async_jobs =
+  let%sub state, set_state = Bonsai.state_opt () in
+  let%sub () =
+    let%sub callback =
+      let%arr set_state = set_state in
+      let%bind.Effect num_jobs =
+        Effect.of_sync_fun
+          (fun () ->
+            ( Async_kernel.Async_kernel_scheduler.Private.num_pending_jobs
+                (Async_kernel.Async_kernel_scheduler.Private.t ())
+            , Async_kernel.Async_kernel_scheduler.Private.num_jobs_run
+                (Async_kernel.Async_kernel_scheduler.Private.t ()) ))
+          ()
+      in
+      set_state (Some num_jobs)
+    in
+    Bonsai.Clock.every
+      ~when_to_start_next_effect:`Every_multiple_of_period_blocking
+      (Time_ns.Span.of_sec 0.1)
+      callback
+  in
+  let%sub sexp_for_debugging = sexp_for_debugging in
+  let%arr state = state
+  and sexp_for_debugging = sexp_for_debugging in
+  sexp_for_debugging [%message (state : (int * int) option)]
+;;
+
+let manga_list =
+  let%sub state, set_state = Bonsai.state_opt () in
+  let%sub () =
+    let%sub on_activate =
+      let%arr set_state = set_state in
+      let%bind.Effect response =
+        Effect.of_deferred_fun
+          (fun () ->
+            (* Result.ok_exn *)
+            (* @@ Async.Thread_safe.block_on_async (fun () -> *)
+            (* let _ : _ = assert false in *)
+            let%map.Async.Deferred x =
+              Mangadex_api.Search.search ~title:"chainsaw"
+            in
+            x)
+          ()
+      in
+      set_state (Some response)
+    in
+    Bonsai.Edge.lifecycle ~on_activate ()
+  in
+  return state
+;;
+
 let content =
   let%sub mauve = Capytui_catpuccin.color Mauve in
   let%sub text = Text.component in
   let%sub spinner = Spinner.component ~kind:Dot (Value.return "") in
-  let%sub () = image in
+  let%sub manga_list = manga_list in
+  let%sub num_async_jobs = num_async_jobs in
+  let%sub sexp_for_debugging = sexp_for_debugging in
   let%arr text = text
   and mauve = mauve
-  and spinner = spinner in
+  and spinner = spinner
+  and manga_list = manga_list
+  and num_async_jobs = num_async_jobs
+  and sexp_for_debugging = sexp_for_debugging in
   let title =
     Node.hcat
       [ text ~attrs:[ Attr.foreground_color mauve; Attr.bold ] "Capymanga"
@@ -50,7 +119,21 @@ let content =
       ; spinner
       ]
   in
-  Node.pad ~l:2 ~t:1 title
+  Node.pad
+    ~l:2
+    ~t:1
+    (Node.vcat
+       [ title
+       ; Node.text ""
+       ; num_async_jobs
+       ; Node.text ""
+       ; sexp_for_debugging
+           [%sexp
+             (manga_list
+              : Mangadex_api.Types.Manga.t Mangadex_api.Types.Collection.t
+                  Or_error.t
+                  option)]
+       ])
 ;;
 
 let app =
@@ -62,9 +145,12 @@ let app =
 ;;
 
 let command =
-  Command.basic
+  let open Async in
+  Command.async_or_error
     ~summary:{|Capy manga!|}
     [%map_open.Command
       let () = return () in
-      fun () -> Capytui.start app]
+      fun () ->
+        Capytui.start app;
+        Deferred.Or_error.return ()]
 ;;
