683da65b6c42d7de54ca943d5cdfa5c3a908793c
enoumy <enoumy@gmail.com>
2024-08-03 12:48:51 -0400

    Removed debug info

--- app/capymanga/src/capymanga.ml
+++ app/capymanga/src/capymanga.ml
@@ -52,24 +52,6 @@ let backdrop =
          (String.make width ' ')))
 ;;
 
-let static_image _dimensions =
-  [ (* { Image.url = *)
-    (* "https://mangadex.org/covers/a77742b1-befd-49a4-bff5-1ad4e6b0ef7b/bdd44bcd-c0dc-4f83-ba15-fc99e8790ed4.jpg" *)
-    (* ; row = 0 *)
-    (* ; column = dimensions.Dimensions.width / 2 *)
-    (* ; dimensions = *)
-    (*     { width = dimensions.width / 2; height = dimensions.height / 2 } *)
-    (* ; scale = true *)
-    (* } *)
-    (* ; { Image.url = "~/pictures/pixel-art-capy.png" *)
-    (*   ; row = dimensions.height / 2 *)
-    (*   ; column = 0 *)
-    (*   ; dimensions = *)
-    (*       { width = dimensions.width / 2; height = dimensions.height / 2 } *)
-    (*   ; scale = true *)
-    (*   } *) ]
-;;
-
 let content =
   let%sub mauve = Capytui_catpuccin.color Mauve in
   let%sub text = Text.component in
@@ -79,8 +61,6 @@ let content =
     | false -> Bonsai.const Node.none
     | true -> Spinner.component ~kind:Spinner.Kind.Dot (Value.return "")
   in
-  (* TODO: Add a spinner. Sadly this messes with the way images are shown, so
-     you should probably implement diffing first... *)
   let%sub table = Manga_table.component in
   let%sub instructions = instructions in
   let%arr text = text
@@ -103,18 +83,12 @@ let content =
 let app =
   Loading_state.register
   @@
-  let%sub image =
-    let%sub dimensions = terminal_dimensions in
-    let%arr dimensions = dimensions in
-    static_image dimensions
-  in
   let%sub content, images = content in
   let%sub backdrop = backdrop in
   let%arr backdrop = backdrop
   and images = images
-  and content = content
-  and image = image in
-  Node.zcat [ content; backdrop ], image @ images
+  and content = content in
+  Node.zcat [ content; backdrop ], images
 ;;
 
 let command =
