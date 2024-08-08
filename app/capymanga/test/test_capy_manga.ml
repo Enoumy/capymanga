1e0ba9f8b502de7021fc0e77c763161a142a1f33
enoumy <enoumy@gmail.com>
2024-08-07 22:37:07 -0400

    Added mock image rendering for tests

--- app/capymanga/test/test_capy_manga.ml
+++ app/capymanga/test/test_capy_manga.ml
@@ -43,6 +43,7 @@ let%expect_test "Initial load" =
   Handle.show handle;
   [%expect
     {|
+    ([manga_search] (title ()))
     ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
     │                                                                                                                        │
     │  Capymanga ⣾  / Search  j Down  k Up                                                                                   │
@@ -86,6 +87,7 @@ let%expect_test "About page" =
   Handle.show handle;
   [%expect
     {|
+    ([manga_search] (title ()))
     ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
     │                                                                                                                        │
     │                                                                                                                        │
@@ -156,6 +158,47 @@ let%expect_test "About page" =
     │                                                                                                                        │
     │    One-Punch Man - Saitama in the One Piece Universe (Doujinshi)  DOUJINSHI   MONSTERS   ACTION   LONG STRIP   COMEDY  │
     │                                                                                                                        │
+    │                                                                                                                        │
+    └────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+    |}];
+  Handle.advance_clock_by handle (Time_ns.Span.of_sec 1.0);
+  Handle.recompute_view handle;
+  Handle.recompute_view handle;
+  [%expect
+    {| ([manga_cover] (cover_id c1037a0c-aa5e-4cd9-bd33-6514a2eb332b)) |}];
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+    │                                                                                                                        │
+    │  Capymanga    / Search  j Down  k Up                       ............................................................│
+    │                                                            ............................................................│
+    │  │ One-Punch Man  AWARD WINNING   SCI-FI   GORE   MONSTERS ............................................................│
+    │  │ After rigorously training for three years, the ordinary.............................................................│
+    │                                                            ............................................................│
+    │    DB X Saitama (Doujinshi)  DOUJINSHI   ACTION   COMEDY   ............................................................│
+    │    Fan-drawn crossover of DBZ and OPM.       **Links:**  ..............................................................│
+    │                                                            ............................................................│
+    │    One-Punch Man - Wash Yourself Before Entering The Bath (............................................................│
+    │    After winning a public bath voucher, Saitama wanted to..............................................................│
+    │                                                            ............................................................│
+    │    One Punch Man (Webcomic/Original)  AWARD WINNING   SCI-F...............((url https://mangadex.org/cov...............│
+    │    Follows the life of an average part-time hero who...    ...............ers/d8a959f7-648e-4c8d-8f23-f1...............│
+    │                                                            ...............f3f8e129f3/0380962e-26f8-4233-...............│
+    │    One-Punch Man - My Student Won't Go Back Home (Doujinshi...............8c24-d36b2ebceb3e.jpg)(row 1)(...............│
+    │    One Punch Man AU, where everything is the same except..................column 60)(dimensions((height ...............│
+    │                                                            ...............28)(width 60)))(scale true)).................│
+    │    One-Punch Man - Because I'm a Monster (Doujinshi)  DOUJI............................................................│
+    │    Fan redraw of chapter 90 of One Punch Man.              ............................................................│
+    │                                                            ............................................................│
+    │    One-Punch Man - Hatachi ni Naru Made Machinasai! (Doujin............................................................│
+    │    Saitama x Genos doujin.                                 ............................................................│
+    │                                                            ............................................................│
+    │    One-Punch Man - The Fight of Gods (Doujinshi)  DOUJINSHI............................................................│
+    │    A fanmade comic by Cminglap, depicting Saitama vs a...  ............................................................│
+    │                                                            ............................................................│
+    │    One-Punch Man - Saitama in the One Piece Universe (Douji............................................................│
+    │                                                            ............................................................│
     │                                                                                                                        │
     └────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
     |}]
--- app/capymanga/test/test_util.ml
+++ app/capymanga/test/test_util.ml
@@ -2,6 +2,7 @@ open! Core
 open! Bonsai
 open Bonsai.Let_syntax
 module Outside_world = Capymanga.For_testing.Outside_world
+open Capytui
 
 let default_manga_search ~title:(_ : string option) =
   Effect.return
@@ -18,12 +19,48 @@ let create_handle
   ?(manga_cover = Value.return default_manga_cover)
   ()
   =
+  let manga_search =
+    let%map manga_search = manga_search in
+    fun ~title ->
+      let%bind.Effect () =
+        Effect.print_s [%message "[manga_search]" (title : string option)]
+      in
+      manga_search ~title
+  in
+  let manga_cover =
+    let%map manga_cover = manga_cover in
+    fun ~cover_id ->
+      let%bind.Effect () =
+        Effect.print_s [%message "[manga_cover]" (cover_id : string)]
+      in
+      manga_cover ~cover_id
+  in
   let app =
     Outside_world.Manga_search.register_mock manga_search
     @@ Outside_world.Manga_cover.register_mock manga_cover
     @@
-    let%sub image, _ = Capymanga.app in
-    Bonsai.read image
+    let%sub image, images = Capymanga.app in
+    let%arr image = image
+    and images = images in
+    let images =
+      List.map images ~f:(fun image ->
+        let string = Sexp.to_string_mach [%sexp (image : Image.t)] in
+        let content =
+          String.to_list string
+          |> List.chunks_of ~length:(image.dimensions.width / 2)
+          |> List.map ~f:(Fn.compose Node.text String.of_char_list)
+          |> Node.vcat
+        in
+        let pad x = Node.pad ~l:image.column ~t:image.row x in
+        Node.zcat
+          [ pad (Node.center ~within:image.dimensions @@ content)
+          ; pad
+              (Node.vcat
+                 (List.init image.dimensions.height ~f:(fun _ ->
+                    Node.text (String.make image.dimensions.width '.'))))
+          ])
+    in
+    Node.zcat (images @ [ image ])
   in
   let handle =
     Capytui_test.create_handle
