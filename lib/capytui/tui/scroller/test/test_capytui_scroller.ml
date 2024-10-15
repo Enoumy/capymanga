22ce035809f8726012a740c0494e0b8430b5a5b4
enoumy <enoumy@gmail.com>
2024-10-14 20:19:27 -0400

    Added tests for less keybindings!

--- lib/capytui/tui/scroller/test/test_capytui_scroller.ml
+++ lib/capytui/tui/scroller/test/test_capytui_scroller.ml
@@ -51,6 +51,15 @@ let component =
     and { left; top } = offset in
     Node.pad ~l:left ~t:top view
   in
+  let%sub () =
+    let%sub listener =
+      let%arr { less_keybindings_handler; inject = _; view = _ } =
+        scroller
+      in
+      less_keybindings_handler
+    in
+    Capytui.listen_to_events listener
+  in
   let%arr scroller = scroller
   and set_dimensions = set_dimensions
   and set_offset = set_offset
@@ -119,7 +128,7 @@ let%expect_test "Sanity printing the handle." =
     |}]
 ;;
 
-let%expect_test "Sanity printing the handle." =
+let%expect_test "Testing the less keybindings" =
   let handle =
     Capytui_test.create_handle_generic
       ~initial_dimensions:{ height = 10; width = 40 }
@@ -132,6 +141,235 @@ let%expect_test "Sanity printing the handle." =
     [ Set_dimensions { height = 5; width = 40 }
     ; Set_offset { left = 0; top = 2 }
     ];
+  Handle.recompute_view handle;
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │0                                       │
+    │1                                       │
+    │2                                       │
+    │3                                       │
+    │4                                       │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  (* Down. *)
+  Capytui_test.send_event handle (`Key (`ASCII 'j', []));
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │1                                       │
+    │2                                       │
+    │3                                       │
+    │4                                       │
+    │5                                       │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  Capytui_test.send_event handle (`Key (`Arrow `Down, []));
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │2                                       │
+    │3                                       │
+    │4                                       │
+    │5                                       │
+    │6                                       │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  (* Up. *)
+  Capytui_test.send_event handle (`Key (`ASCII 'k', []));
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │1                                       │
+    │2                                       │
+    │3                                       │
+    │4                                       │
+    │5                                       │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  Capytui_test.send_event handle (`Key (`Arrow `Up, []));
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │0                                       │
+    │1                                       │
+    │2                                       │
+    │3                                       │
+    │4                                       │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  (* Half page down. *)
+  Capytui_test.send_event handle (`Key (`ASCII 'd', [ `Ctrl ]));
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │2                                       │
+    │3                                       │
+    │4                                       │
+    │5                                       │
+    │6                                       │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  Capytui_test.send_event handle (`Key (`ASCII 'd', []));
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │4                                       │
+    │5                                       │
+    │6                                       │
+    │7                                       │
+    │8                                       │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  (* Half page up. *)
+  Capytui_test.send_event handle (`Key (`ASCII 'u', [ `Ctrl ]));
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │2                                       │
+    │3                                       │
+    │4                                       │
+    │5                                       │
+    │6                                       │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  Capytui_test.send_event handle (`Key (`ASCII 'u', []));
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │0                                       │
+    │1                                       │
+    │2                                       │
+    │3                                       │
+    │4                                       │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  (* Bottom. *)
+  Capytui_test.send_event handle (`Key (`ASCII 'G', []));
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │999                                     │
+    │                                        │
+    │                                        │
+    │                                        │
+    │                                        │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  (* Top. *)
+  Fn.apply_n_times
+    ~n:2
+    (fun () -> Capytui_test.send_event handle (`Key (`ASCII 'g', [])))
+    ();
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │0                                       │
+    │1                                       │
+    │2                                       │
+    │3                                       │
+    │4                                       │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  (* Top - with a time delay! *)
+  Fn.apply_n_times
+    ~n:100
+    (fun () -> Capytui_test.send_event handle (`Key (`ASCII 'j', [])))
+    ();
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │100                                     │
+    │101                                     │
+    │102                                     │
+    │103                                     │
+    │104                                     │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  Capytui_test.send_event handle (`Key (`ASCII 'g', []));
+  Handle.recompute_view handle;
+  Handle.advance_clock_by handle (Time_ns.Span.of_sec 0.3);
+  Capytui_test.send_event handle (`Key (`ASCII 'g', []));
+  (* No difference! Waiting 0.3 seconds was too long! *)
+  Handle.show handle;
+  [%expect
+    {|
+    ┌────────────────────────────────────────┐
+    │                                        │
+    │                                        │
+    │100                                     │
+    │101                                     │
+    │102                                     │
+    │103                                     │
+    │104                                     │
+    │                                        │
+    │                                        │
+    └────────────────────────────────────────┘
+  |}];
+  Handle.advance_clock_by handle (Time_ns.Span.of_sec 0.295);
+  Handle.recompute_view handle;
+  (* Waiting 0.295 seconds in between is fine! *)
+  Capytui_test.send_event handle (`Key (`ASCII 'g', []));
   Handle.show handle;
   [%expect
     {|
